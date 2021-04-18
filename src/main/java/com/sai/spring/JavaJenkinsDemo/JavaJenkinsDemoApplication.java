7a5027a09eb46374838f6f0331c7beff61cf2488
skr7 <sai.tokachichu@gmail.com>
2021-04-18 16:45:34 +0530

    Adding HelloWorldController

---
+++ src/main/java/com/sai/spring/JavaJenkinsDemo/JavaJenkinsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sai.spring.JavaJenkinsDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaJenkinsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaJenkinsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sai/spring/JavaJenkinsDemo/JavaJenkinsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sai.spring.JavaJenkinsDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaJenkinsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
